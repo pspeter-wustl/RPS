d2b49d2b8a84d93c39454c4ce6b46115d13b12bb
Phips Peter <phips3@gmail.com>
2010-11-16 14:27:16 -0600

    added DB requirements to profile.rb

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,5 +2,11 @@
 # The profile class is used to track information for a profile including their
 # name and win/loss statistics.
 
+require 'sqlite3'
+
 class Profile
+    # Constructor, takes the name of a user
+    def initialize(name)
+      
+    end
 end
