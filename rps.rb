8c65f9d6691e265908375f9c86480fb217a59795
Phips Peter <pspeter@wustl.edu>
2010-12-06 00:09:47 -0600

    created options hash

--- rps.rb
+++ rps.rb
@@ -2,28 +2,16 @@
 # == Synopsis 
 #   This app plays Rock, Paper, Scissors with users and hopes to win against
 #   the average player not using a random strategy.
-#
-# == Usage 
-#   rps [options]
-#
-#   For help use: rps -h
-#
-# == Options
-#   -h, --help          Displays help message
-#   -c, --computer      Disables analyzing the computer
-#   -u, --user          Disables analyzing the user 
-#   -s, --search        Search pattern length, default is 5
-#   -p, --past          How far to look back into the past, default is 50
-#
-# == Author
-#   Phips Peter
-
-require 'app/controllers/engine'
-require 'optparse'
 
 # Main program
 puts "Rock Paper Scissors.rb\nWhat is your name?"
 print ">> "
 name = gets
-engine = Engine.new(name, {:analyze_computer => false})
+engine = Engine.new(name, 
+  {
+  :max_search => 5,
+  :max_history => 50,
+  :analyze_user => true,
+  :analyze_computer => false,
+  })
 engine.run!
